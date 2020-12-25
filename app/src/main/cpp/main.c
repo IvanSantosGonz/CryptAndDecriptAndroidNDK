42dbc890379cffde2f9c48b5a9e219aebe311e90
Ivan <Ivansantosgonz@gmail.com>
2020-12-25 20:18:30 +0000

    encrypt/decrypt working from c

--- app/src/main/cpp/main.c
+++ app/src/main/cpp/main.c
@@ -12,51 +12,64 @@
 #include <string.h>
 #include <openssl/evp.h>
 
-JNIEXPORT jstring JNICALL
-Java_com_samplendkapp_MainActivity_stringFromJNI(JNIEnv *env, jobject a, jstring message) {
-    __android_log_print(ANDROID_LOG_DEBUG, "LOG_TAG", "Log from main.c ");
 
-    /* A 256 bit key */
-    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+/* A 256 bit key */
+unsigned char *key = (unsigned char *) "01234567890123456789012345678901";
 
-    /* A 128 bit IV */
-    unsigned char *iv = (unsigned char *)"0123456789012345";
+/* A 128 bit IV */
+unsigned char *iv = (unsigned char *) "0123456789012345";
+
+JNIEXPORT jbyteArray JNICALL
+Java_com_samplendkapp_MainActivity_encryptFromJNI(JNIEnv *env, jobject thiz, jstring message) {
+
+    int ciphertext_len;
+/*
+ * Buffer for ciphertext. Ensure the buffer is long enough for the
+ * ciphertext which may be longer than the plaintext, depending on the
+ * algorithm and mode.
+ */
+    unsigned char ciphertext[128];
 
     /* Message to be encrypted */
     unsigned char *plaintext = (*env)->GetStringUTFChars(env, message, 0);
 
-    /*
-     * Buffer for ciphertext. Ensure the buffer is long enough for the
-     * ciphertext which may be longer than the plaintext, depending on the
-     * algorithm and mode.
-     */
-    unsigned char ciphertext[128];
+    /* Encrypt the plaintext */
+    ciphertext_len = encrypt(plaintext, strlen((char *) plaintext), key, iv,
+                             ciphertext);
+    printf("Ciphertext is:\n");
+    BIO_dump_fp(stdout, (const char *) ciphertext, ciphertext_len);
 
-    /* Buffer for the decrypted text */
+    __android_log_print(ANDROID_LOG_DEBUG, "LOG_TAG", "Log from main.c %s", ciphertext);
+
+    jbyteArray array = (*env)->NewByteArray(env, ciphertext_len);
+    (*env)->SetByteArrayRegion(env, array, 0, ciphertext_len, (jbyte *) (ciphertext));
+    return array;
+
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_samplendkapp_MainActivity_decryptFromJNI(JNIEnv *env, jobject thiz, jbyteArray message) {
+
+    int decryptedtext_len;
+
+/* Buffer for the decrypted text */
     unsigned char decryptedtext[128];
 
-    int decryptedtext_len, ciphertext_len;
+    /* Message to be encrypted */
+    int len = (*env)->GetArrayLength (env, message);
+    unsigned char encryptedText[len];
+    (*env)->GetByteArrayRegion (env, message, 0, len, (jbyte*) (encryptedText));
 
-    /* Encrypt the plaintext */
-    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
-                              ciphertext);
-
-    /* Do something useful with the ciphertext here */
-    printf("Ciphertext is:\n");
-    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+    __android_log_print(ANDROID_LOG_DEBUG, "LOG_TAG", "Log from decrypt %s ", encryptedText);
 
     /* Decrypt the ciphertext */
-    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+    decryptedtext_len = decrypt(encryptedText, len, key, iv,
                                 decryptedtext);
 
     /* Add a NULL terminator. We are expecting printable text */
     decryptedtext[decryptedtext_len] = '\0';
 
-    /* Show the decrypted text */
-    printf("Decrypted text is:\n");
-    printf("%s\n", decryptedtext);
-
-
     return (*env)->NewStringUTF(env, decryptedtext);
+
 
 }
--- app/src/main/java/com/samplendkapp/MainActivity.kt
+++ app/src/main/java/com/samplendkapp/MainActivity.kt
@@ -1,23 +1,50 @@
 package com.samplendkapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var encryptedText = findViewById<TextView>(R.id.text_encrypted_decrypted)
+
         // Example of a call to a native method
-        findViewById<TextView>(R.id.text_title).text = stringFromJNI("PEPE")
+        var encryptButton = findViewById<Button>(R.id.button_encrypt)
+        encryptButton.setOnClickListener {
+            run {
+                val plainText = findViewById<EditText>(R.id.encrypt_decrypt_text).text.toString()
+                val encryptedBytes = encryptFromJNI(plainText)
+                encryptedText.text = Base64.getEncoder().encodeToString(encryptedBytes)
+                encryptedText.visibility = View.VISIBLE
+            }
+        }
+
+        var decryptedText = findViewById<TextView>(R.id.text_encrypted_decrypted)
+
+        var decryptButton = findViewById<Button>(R.id.button_decrypt)
+        decryptButton.setOnClickListener {
+            run {
+                val cipherText = findViewById<EditText>(R.id.encrypt_decrypt_text).text.toString()
+
+                decryptedText.text = decryptFromJNI(Base64.getDecoder().decode(cipherText))
+                decryptedText.visibility = View.VISIBLE
+            }
+        }
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    private external fun stringFromJNI(message: String): String
+    private external fun encryptFromJNI(message: String): ByteArray
+    private external fun decryptFromJNI(message: ByteArray): String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
